197aff582c069b45706916a35f9b4607a996c28a
thurcruz <arthurindcruz@gmail.com>
2026-03-06 17:58:43 -0300

    desbug planos

--- app/gerenciar-plano/page.tsx
+++ app/gerenciar-plano/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
-import GerenciarPlanoClient from './GerenciarPlanoCliente'
+import GerenciarPlanoClient from './GerenciarPlanoClient'
 
 export default async function PaginaGerenciarPlano() {
   const supabase = await createClient()
